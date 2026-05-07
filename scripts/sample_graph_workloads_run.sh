33ec13385d774941bae709483c735707296374f1
javathunderman <javathunderman@outlook.com>
2026-05-07 07:00:59 +0000

    patch sim files

--- scripts/sample_graph_workloads_run.sh
+++ scripts/sample_graph_workloads_run.sh
@@ -1,4 +1,4 @@
-g++ -O0 \-nostdlib -static -ffreestanding -fno-exceptions -fno-rtti -fno-stack-protector -no-pie /ezh/workloads/graph_workloads/graph_coarsening.cpp \-I/ezh/gem5/include \-L/ezh/gem5/util/m5/build/x86/out -lm5 -o graph_coarsening
+g++ -O0 \-nostdlib -static -ffreestanding -fno-exceptions -fno-rtti -fno-stack-protector -no-pie /ezh/workloads/graph_workloads/graph_coarsening.cpp \-I/ezh/gem5/include \-L/ezh/gem5/util/m5/build/x86/out -lm5 -o /ezh/workloads/graph_workloads/graph_coarsening
 cd ../gem5 && build/X86/gem5.opt \--debug-flags=DRAMOpt \--outdir=/ezh/workloads/graph_workloads/m5out_graph_coarsening/ \configs/example/se.py   \--cpu-type=AtomicSimpleCPU   \--cpu-clock=4GHz   \--cacheline_size=64   \--num-cpus=1   \--cmd=/ezh/workloads/graph_workloads/graph_coarsening > /ezh/workloads/graph_workloads/graph_coarsening_log.txt
 cd ../scripts
 python3 /ezh/gem5/util/decode_packet_trace.py /ezh/workloads/graph_workloads/m5out_graph_coarsening/trace.ptrc.gz /ezh/workloads/graph_workloads/graph_coarsening_log_raw_trace
@@ -11,7 +11,7 @@ mkdir -p /ezh/workloads/graph_workloads/chunk_traces
 for i in {0..6..2}
 do
     if [ "$i" -ne 0 ]; then
-        python3 /ezh/scripts/rewrite_dramsim3_bankshift.py --indir /ezh/workloads/graph_workloads/chunk_traces/graph_coarsening_trace_chunks_bitshift_0/ --outdir /ezh/workloads/graph_workloads/chunk_traces/graph_coarsening_trace_chunks_bitshift_$i --bank-count 16 --bank-lsb 0 --bank-bit-shift $i --iter-set 5
+        python3 /ezh/scripts/rewrite_dramsim3_bankshift.py --indir /ezh/workloads/graph_workloads/chunk_traces/graph_coarsening_trace_chunks_bitshift_0/ --outdir /ezh/workloads/graph_workloads/chunk_traces/graph_coarsening_trace_chunks_bitshift_$i --bank-count 16 --bank-lsb 0 --bank-bit-shift $i --iter-set 5 --clobber
     fi
     python3 /ezh/scripts/set_trace_cycle_zero.py /ezh/workloads/graph_workloads/chunk_traces/graph_coarsening_trace_chunks_bitshift_$i
     for j in {0..5}
@@ -32,3 +32,4 @@ do
     done
     # ./ezh/DRAMsim3/build/dramsim3main ../configs/DDR4_8Gb_x8_3200.ini -c 807816 -t /ezh/workloads/graph_workloads/chunk_traces/graph_coarsening_trace_chunks_$i/iter_000006_cycles_792649_807816.trace -o /ezh/workloads/graph_workloads/dramsim_results/dramsim_results_bitshift_$i/dramsim_results_6
 done
+mv dram_opt_markers* workloads/graph_workloads
