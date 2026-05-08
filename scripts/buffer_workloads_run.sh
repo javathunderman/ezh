5bc3a9514414dd47de5ecedc61263e4ffd8a3935
VerdantLeaf <kylej6@illinois.edu>
2026-05-08 16:16:39 -0500

    Same results for when buffering is enabled, at least on graph

--- scripts/buffer_workloads_run.sh
+++ scripts/buffer_workloads_run.sh
@@ -14,12 +14,22 @@ WORKLOAD_NAME=graph_coarsening
 WORKLOAD_BIN=$WORKLOAD_DIR/$WORKLOAD_NAME
 WORKLOAD_SRC=$WORKLOAD_DIR/$WORKLOAD_NAME.cpp
 
-M5OUT_DIR=$WORKLOAD_DIR/m5out_$WORKLOAD_NAME
+M5OUT_DIR=$WORKLOAD_DIR/m5out_${WORKLOAD_NAME}
 RAW_TRACE=$WORKLOAD_DIR/${WORKLOAD_NAME}_log_raw_trace
 DRAMSIM_TRACE=$WORKLOAD_DIR/${WORKLOAD_NAME}_dramsim3.trace
-DRAMSIM_RESULTS=$WORKLOAD_DIR/dramsim_results
 
-DRAM_CONFIG=DDR4_8Gb_x8_3200
+DRAMSIM_RESULTS_DIR=$WORKLOAD_DIR/dramsim_results
+
+DRAMSIM_RESULTS=$DRAMSIM_RESULTS_DIR/unspecified
+UNIFIED_DRAMSIM_RESULTS=$DRAMSIM_RESULTS_DIR/unified_queue
+NONUNIFIED_DRAMSIM_RESULTS=$DRAMSIM_RESULTS_DIR/non_unified_queue
+
+DRAM_CONFIG=DDR4_8Gb_x8_3200          # unspecified unified queue
+
+# These are the most architecturally similar pairs that exist in DRAM sim naturally
+# DRAM_CONFIG=HBM1_4Gb_x128            # unspecified unified queue
+# UNIFIED_DRAM_CONFIG=HMC_2GB_4Lx16    # unified queue config
+# NONUNIFIED_DRAM_CONFIG=HBM2_4Gb_x128 # non-unified queue config
 
 # echo "Source:   $WORKLOAD_SRC"
 # echo "Include:  $GEM5_DIR/include"
@@ -65,12 +75,29 @@ echo "===================== Trace conversion complete ====================="
 final_cycle=$(tail -n 1 $DRAMSIM_TRACE | awk '{print $3}')
 
 mkdir -p $DRAMSIM_RESULTS
+mkdir -p $UNIFIED_DRAMSIM_RESULTS
+mkdir -p $NONUNIFIED_DRAMSIM_RESULTS
+
+# RUN EXPERIMENTS FORALL THREE QUEUE TYPES
+
 $DRAMSIM_DIR/build/dramsim3main \
     $DRAMSIM_DIR/configs/$DRAM_CONFIG.ini \
     -c $final_cycle \
     -t $DRAMSIM_TRACE \
-    -o $DRAMSIM_RESULTS \
-    --enable_buffering
+    -o $DRAMSIM_RESULTS
 
-echo "===================== DRAMSim simulation complete ====================="
+$DRAMSIM_DIR/build/dramsim3main \
+    $DRAMSIM_DIR/configs/${DRAM_CONFIG}_unified.ini \
+    -c $final_cycle \
+    -t $DRAMSIM_TRACE \
+    -o $UNIFIED_DRAMSIM_RESULTS
+
+$DRAMSIM_DIR/build/dramsim3main \
+    $DRAMSIM_DIR/configs/${DRAM_CONFIG}_non_unified.ini \
+    -c $final_cycle \
+    -t $DRAMSIM_TRACE \
+    -o $NONUNIFIED_DRAMSIM_RESULTS
+
+echo "===================== DRAMSim simulations complete ====================="
+
 
